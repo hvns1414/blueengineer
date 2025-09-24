a1cad63edbc046ce08666ce4322f1bd0397a8a6e
sh3dow <miracberktasdemir202@gmail.com>
2025-09-24 18:54:24 +0300

    Create install.bat

---
+++ install.bat
@@ -0,0 +1,7 @@
+@echo off
+echo Installing Python requirements...
+pip install -r requirements.txt
+echo Installation complete!
+echo Run the checker as Administrator:
+echo python checker_firewall_hash_crossplatform_en.py
+pause
